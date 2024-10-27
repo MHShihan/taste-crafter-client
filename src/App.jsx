ec7a6ca8623114335e635e9bebedc03f734056f5
MHShihan <mahmodul.no.1@gmail.com>
2024-10-27 22:36:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl font-bold text-purple-500 text-center mt-20">
+        Hello Everyone from Taste Crafter Restaurant
+      </h1>
+    </>
+  );
+}
+
+export default App;
