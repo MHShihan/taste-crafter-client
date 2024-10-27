48a99f615e87bf1f9c9a6ea4cc20e3c4ca40cdff
MHShihan <mahmodul.no.1@gmail.com>
2024-10-27 22:45:16 +0600

    Update some content

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ function App() {
       <h1 className="text-5xl font-bold text-purple-500 text-center mt-20">
         Hello Everyone from Taste Crafter Restaurant
       </h1>
+      <h1 className="text-5xl font-bold text-purple-500 text-center mt-20">
+        Hello Everyone from Taste Crafter Restaurant
+      </h1>
+      <h1 className="text-5xl font-bold text-purple-500 text-center mt-20">
+        Hello Everyone from Taste Crafter Restaurant
+      </h1>
     </>
   );
 }
